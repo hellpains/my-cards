c5278bec96360f266a613d5f512691283a8f86f1
hellpains <rustam2004sadulaev@mail.ru>
2022-07-24 19:16:15 +0300

    add thunk

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,12 +1,12 @@
-import {AnyAction, combineReducers, createStore} from "redux";
-import {ThunkAction, ThunkDispatch} from "redux-thunk";
+import {AnyAction, applyMiddleware, combineReducers, createStore} from "redux";
+import thunk, {ThunkAction, ThunkDispatch} from "redux-thunk";
 import {TypedUseSelectorHook, useDispatch, useSelector} from "react-redux";
 
 export const rootReducer = combineReducers({
 
 })
 export type AppRootStateType = ReturnType<typeof rootReducer>
-export const store = createStore(rootReducer)
+export const store = createStore(rootReducer,applyMiddleware(thunk))
 
 
 
